efa367b9cc183d1c240082016f47cba906ef73fa
Joey Massana <jose_massana07@outlook.com>
2024-09-17 22:07:24 -0500

    launched web app with dataframe

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-import streamlit as st
+# Import packages
 
-st.write("Hello world")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv("Employee.csv")    # Created dataframe df using Employee.csv
+st.write(df)                        # Ran dataframe into Streamlit web app
