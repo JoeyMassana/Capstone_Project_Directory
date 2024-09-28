c974ae3946efde262b901c9dd367701d6e514e92
Joey Massana <jose_massana07@outlook.com>
2024-09-27 21:30:13 -0500

    Started Scatter Plot comparing attrtion rates with Years since last Promotion columns

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Created and merged DataFrames.
 pr_df = pd.read_csv("PerformanceRating.csv")
@@ -11,3 +13,4 @@ merged_df = pr_df.merge(emp_df, how='outer', on='EmployeeID')
 # Calculated number of employees with no performance data.
 NullEmployees = merged_df["PerformanceID"].isnull().sum()
 
+st.scatter_chart(data=merged_df, x='YearsSinceLastPromotion', y='Attrition', x_label='Years Since Last Promotion', y_label='Attrtion', color=None, size=None, width=None, height=None, use_container_width=True)
