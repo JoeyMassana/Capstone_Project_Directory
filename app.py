8782352e705b22906b1b9beebfd4dbbd20fbb00c
Joey Massana <jose_massana07@outlook.com>
2024-09-29 10:27:01 -0500

    Unable to label x- and y- axis in scatter plot.

--- app.py
+++ app.py
@@ -13,4 +13,3 @@ merged_df = pr_df.merge(emp_df, how='outer', on='EmployeeID')
 # Calculated number of employees with no performance data.
 NullEmployees = merged_df["PerformanceID"].isnull().sum()
 
-st.scatter_chart(data=merged_df, x='YearsSinceLastPromotion', y='Attrition', x_label='Years Since Last Promotion', y_label='Attrtion', color=None, size=None, width=None, height=None, use_container_width=True)
